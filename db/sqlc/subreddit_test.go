b9af99b52c5c31f776eae123bb9b8c566b929c35
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-04-10 17:19:45 +0200

    added tests for subreddit CRUD operations

---
+++ db/sqlc/subreddit_test.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/bxcodec/faker/v3"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomSubreddit(t *testing.T) Subreddit {
+	user := createRandomUser(t)
+	require.NotEmpty(t, user)
+
+	arg := CreateSubredditParams{
+		Name:      faker.Name(),
+		Moderator: user.Username,
+		Description: sql.NullString{
+			String: faker.Paragraph(),
+			Valid:  true,
+		},
+	}
+
+	subreddit, err := testQueries.CreateSubreddit(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, subreddit.Name)
+
+	require.Equal(t, arg.Name, subreddit.Name)
+	require.Equal(t, arg.Moderator, subreddit.Moderator)
+	require.Equal(t, arg.Description, subreddit.Description)
+
+	return subreddit
+}
+
+func TestCreateSubreddit(t *testing.T) {
+	createRandomSubreddit(t)
+}
+
+func TestGetSubreddit(t *testing.T) {
+	subreddit1 := createRandomSubreddit(t)
+	require.NotEmpty(t, subreddit1)
+
+	subreddit2, err := testQueries.GetSubreddit(context.Background(), subreddit1.Name)
+	require.NoError(t, err)
+
+	require.Equal(t, subreddit1.Name, subreddit2.Name)
+	require.Equal(t, subreddit1.Moderator, subreddit2.Moderator)
+	require.Equal(t, subreddit1.Description, subreddit2.Description)
+	require.Equal(t, subreddit1.Followers, subreddit2.Followers)
+	require.Equal(t, subreddit1.CreatedAt, subreddit2.CreatedAt)
+}
+
+func TestDeleteSubreddit(t *testing.T) {
+	subreddit1 := createRandomSubreddit(t)
+	require.NotEmpty(t, subreddit1)
+
+	err := testQueries.DeleteSubreddit(context.Background(), subreddit1.Name)
+	require.NoError(t, err)
+
+	subreddit2, err := testQueries.GetSubreddit(context.Background(), subreddit1.Name)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, subreddit2)
+}
